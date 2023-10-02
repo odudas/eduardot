c005635f02d9653ff397261da965841d31c475af
Eduardo Tavares <eduardot.dev@icloud.com>
2023-10-02 15:48:23 -0300

    add experiences page

---
+++ src/app/experiencias/page.tsx
@@ -0,0 +1,21 @@
+import { Clipboard } from 'lucide-react'
+
+export default function Experiences() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center gap-4 text-center">
+        <div>
+          <h3 className="text-xl font-bold text-zinc-700">
+            Ainda não possuo nenhuma experiência profissional como dev
+          </h3>
+          <span className="font-medium text-zinc-500">
+            Mas conto com você para me ajudar a conquistar este espaço e
+            preencher esta página!
+          </span>
+        </div>
+
+        <Clipboard className="h-44 w-36" strokeWidth={1} />
+      </div>
+    </>
+  )
+}
